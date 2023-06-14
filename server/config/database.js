2150a48f120701e6a1ce713b415dc1166e82283e
Prankush Giri <76916192+prankush-tech@users.noreply.github.com>
2023-06-14 19:13:44 +0530

    🪲test

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,10 @@
 import mongoose from 'mongoose';
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
-export const connectDB = async()=>{
+const connectDB = async () => {
+	const { connection } = await mongoose.connect(process.env.MONGO_URI);
 
-    const {connection} = await mongoose.connect(
-        process.env.MONGO_URI,
-
-    )
-    
-    console.log(`Database is Connceted with: ${connection.host}`)
-
-}
+	console.log(`Database is Connceted with: ${connection.host}`);
+};
+export default connectDB;
